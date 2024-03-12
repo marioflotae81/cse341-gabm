cb767770a43ea599b18f154e8460a4f291e18905
Mario <mario@hfrocks.com>
2024-03-12 14:49:57 -0600

    Connection to MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const { connectToMongoDB } = require('./db/index');
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 });
 
-const port = 3003;
-app.listen(process.env.PORT || port);
+const port = process.env.PORT || 3003;
 
-console.log('Web Server is listening at port ' + (process.env.PORT || port));
+
+connectToMongoDB()
+.then(() => {
+        app.listen(port, () => {
+            console.log('Web Server is listening at port ' + (port));
+        });
+
+    })
+    .catch(() => {
+        console.error('Error connecting to MongoDB', error);
+    })
